42144e918f1f21fac31af489f415d801aef482b6
SarveshLimaye <sarveshlimaye2002@gmail.com>
2024-10-21 23:21:46 +0530

    Social auth using particle and rainbow kit

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
+import WagmiProvider from "@/utils/wagmiProvider";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiProvider>
+      <Component {...pageProps} />
+    </WagmiProvider>
+  );
 }
