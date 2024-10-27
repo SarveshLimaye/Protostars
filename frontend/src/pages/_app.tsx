31d2dad886a941c2686931960b2b144c4604dc7a
Mukul Kolpe <mukulkolpe45@gmail.com>
2024-10-27 15:08:28 +0530

    Fixed hydration errors

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,7 +1,11 @@
-import Navbar from "@/components/NavBar/NavBar";
+import dynamic from "next/dynamic";
 import "@/styles/globals.css";
 import WagmiProvider from "@/utils/wagmiProvider";
 import type { AppProps } from "next/app";
+
+const Navbar = dynamic(() => import("@/components/Navbar/NavBar"), {
+  ssr: false,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
