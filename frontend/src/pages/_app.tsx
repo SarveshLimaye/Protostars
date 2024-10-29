adee693c369c3e9f9000ae696246eb45c56e3df7
Sarvesh Limaye <74766567+SarveshLimaye@users.noreply.github.com>
2024-10-29 17:11:12 +0530

    Import issue solved for navbar (#22)

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,10 @@
-// @ts-nocheck comment
 import dynamic from "next/dynamic";
 import "@/styles/globals.css";
 import WagmiProvider from "@/utils/wagmiProvider";
 import type { AppProps } from "next/app";
 import Footer from "@/components/Footer/Footer";
 
-const Navbar = dynamic(() => import("@/components/Navbar/NavBar"), {
+const Navbar = dynamic(() => import("@/components/NavBar/NavBar"), {
   ssr: false,
 });
 
