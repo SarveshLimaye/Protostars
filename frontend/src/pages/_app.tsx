93c6f069b0138cc091388d885f414b6e2213fa97
Mukul Kolpe <mukulkolpe45@gmail.com>
2024-11-04 03:15:48 +0530

    Updated website metadata

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "@/styles/globals.css";
 import WagmiProvider from "@/utils/wagmiProvider";
 import type { AppProps } from "next/app";
 import Footer from "@/components/Footer/Footer";
+import Head from "next/head";
 
 const Navbar = dynamic(() => import("@/components/NavBar/NavBar"), {
   ssr: false,
@@ -10,10 +11,19 @@ const Navbar = dynamic(() => import("@/components/NavBar/NavBar"), {
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <WagmiProvider>
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
-    </WagmiProvider>
+    <>
+      <Head>
+        <title>SkillSphere</title>
+        <meta
+          name="description"
+          content="Showcase Your Skills, Protect Your Identity"
+        />
+      </Head>
+      <WagmiProvider>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </WagmiProvider>
+    </>
   );
 }
